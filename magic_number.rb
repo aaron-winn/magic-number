1a7e09bd5195ba04aaf8180ed3cb2e09dfdef88a
Aaron_Winn <winn403@hotmail.com>
2018-01-08 14:09:34 -0700

    init

---
+++ magic_number.rb
@@ -0,0 +1,16 @@
+require 'to_words'
+
+# call for prog
+def magic
+# call for number
+def your_number
+    puts "what is your number?"  
+#   
+    input = gets.strip.to_words
+end
+# start running numbers
+
+#
+
+
+
